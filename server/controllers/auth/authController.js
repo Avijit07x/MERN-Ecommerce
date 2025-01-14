a21d97be2ad41e70104950e896e1e25838637970
Avijit Dey <deyavijit134@gmail.com>
2025-01-14 15:58:08 +0530

    ad

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -31,7 +31,10 @@ const registerUser = async (req, res) => {
 		}
 
 		// hash password
-		const hashedPassword = await bcrypt.hash(password, 5);
+		const hashedPassword = await bcrypt.hash(
+			password,
+			process.env.BCRYPT_SALT_ROUNDS
+		);
 		const user = { username, email, password: hashedPassword };
 
 		// save new user
